813c4e050e1a108d97b4570399a649ffbbc3e6aa
jipark96 <wlgus996@naver.com>
2023-06-15 11:01:35 +0900

    4

--- NumberBaseballGame.java
+++ NumberBaseballGame.java
@@ -9,7 +9,7 @@ public class NumberBaseballGame {
         int[] computerNumbers = new int[4];
         for (int i = 0; i < 4; i++) {
             computerNumbers[i] = rand.nextInt(9) + 1; // Generates a number between 1 and 9.
-            for (int j = 0; j < i; j++) { // Make sure there are no duplicate numbers!
+            for (int j = 0; j < i; j++) { // Make sure there are no duplicate numbers.
                 if (computerNumbers[i] == computerNumbers[j]) {
                     i--;
                     break;
@@ -18,7 +18,7 @@ public class NumberBaseballGame {
         }
 
         while (true) {
-            System.out.println("Please enter three numbers:");
+            System.out.println("Please enter four numbers:");
             int[] playerNumbers = new int[4];
             for (int i = 0; i < 4; i++) {
                 try {
